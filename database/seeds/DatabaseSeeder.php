711f3e4c50fff41cca008e5c09ace164922dca65
sashenme <sashenpasindu@gmail.com>
2020-04-22 23:13:27 +0530

    Make default users seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,8 +16,24 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
 
-       Role::create(['name' => 'user']);
-       Role::create(['name' => 'admin']);
+        Role::create(['name' => 'user']);
+        Role::create(['name' => 'admin']);
+
+        User::create([
+            'first_name' => 'Default',
+            'last_name' => 'Admin',
+            'employee_id' => '0000',
+            'email' => 'admin@pluto.lk',
+            'password' => bcrypt('WishWell')
+        ]);
+
+        User::create([
+            'first_name' => 'Default',
+            'last_name' => 'User',
+            'employee_id' => '0001',
+            'email' => 'user@pluto.lk',
+            'password' => bcrypt('password')
+        ]);
 
         $user = User::find(1);
         $user->assignRole('admin');
