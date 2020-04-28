f863e803919d33d7cf3f3518c9b8c8bf0ef6c522
sashenme <sashenpasindu@gmail.com>
2020-04-29 02:38:52 +0530

    Add new ui to daily quiz

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -45,14 +45,13 @@
         </div>
     </nav>
     @yield('content')
+    <div class="clearfix"></div>
     <footer class="mt-5">
         <div class="d-flex justify-content-between  main-container">
             <div class="p-2 bd-highlight">Pluto Pro &copy; 2020</div>
             <div class="p-2 bd-highlight">Engineered by <a href="#">Innovus.lk</a></div>
         </div>
     </footer>
-
-
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
--- resources/views/user/dailyQuiz.blade.php
+++ resources/views/user/dailyQuiz.blade.php
@@ -1,46 +1,39 @@
-@extends('layouts.admin')
+@extends('layouts.user')
 
 @section('content')
-<div class="row clearfix">
-    <div class="col-lg-12">
-        <div class="card card-success">
-            <div class="card-header">
-                <h3 class="card-title">Daily Questions</h3>
-
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
-                    </button>
-                </div>
-                <!-- /.card-tools -->
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body" style="display: block;">
-            @if(session()->has('status'))
-            <div class="alert alert-{{session('status')}}">
-            {{ session('message') }}
-            </div>
-            @endif
-                <form action="{{route('responses.store')}}" method="POST">
-                {{csrf_field()}}
-                    
-                    <h5>{{$questions->title}}</h5>
-                    <input type="hidden" name="question_id" value="{{$questions->id}}">
-                    <?php $answers = App\Answer::where('question_id', $questions->id)->get() ?>
-                    @foreach($answers as $answer)
-                    <div class="custom-control custom-checkbox">
-                        <input class="custom-control-input" type="radio" id="answer-{{$answer->id}}" name="answer_id" value="{{$answer->id}}">
-                        <label for="answer-{{$answer->id}}" class="custom-control-label">{{$answer->name}}</label>
-                    </div>
-                    @endforeach
-
-                   
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-success">Submit</button>
-                    </div>
-                </form>
-            </div>
-            <!-- /.card-body -->
+<div class="bg-wrap-top">
+    <div class="container-fluid main-container d-flex justify-content-between">
+        <div>
+            <h3 class="mb-0">Daily Quiz</h3>
+            <h1 class="mb-0">Hand Washing</h1>
         </div>
+        <h1 class="mt-3">01</h1>
     </div>
+</div>
+<div class="container-fluid main-container">
+    @if(session()->has('status'))
+    <div class="alert alert-{{session('status')}}">
+        {{ session('message') }}
+    </div>
+    @endif
+
+    <form action="{{route('responses.store')}}" method="POST">
+
+        <input type="hidden" name="question_id" value="{{$questions->id}}">
+        <?php $answers = App\Answer::where('question_id', $questions->id)->get() ?>
+
+        <h2 class="text-center my-5">{{$questions->title}}</h2>
+        <div class="answers text-center">
+            @foreach($answers as $answer)
+            <input class="" type="radio" id="answer-{{$answer->id}}" name="answer_id" value="{{$answer->id}}">
+            <!-- <label class="custom-control-label"></label> -->
+            <label class="answer" for="answer-{{$answer->id}}">{{$answer->name}}</label>
+            @endforeach
+        </div>
+        {{csrf_field()}}
+
+
+        <button type="submit" class="btn btn-primary text-center d-block mx-auto mt-5">Submit</button>
+    </form>
 </div>
 @endsection
