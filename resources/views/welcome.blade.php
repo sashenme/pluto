ed2c5a7ea98369f397f1f02210b1f948b102c7ed
unknown <Udith Shan@MDS-LT-ATO030.mica.grp.local>
2020-04-30 00:45:13 +0530

    Welcome Page Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,51 +12,64 @@
     </head>
 
 <body>
-
-<div class="homebox">
-    <div class="row">
-
+<div class="row">
+    <div class="homebox col-md-11 col-sm-12">
+        <div class="clearfix">
+             <div class="float-left">
+                 <nav class="nav-home navbar-expand-lg navbar-light">
+                     <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+                         <span class="navbar-toggler-icon"></span>
+                     </button>
+                     <div id="my-nav" class="collapse navbar-collapse">
+                         <ul class="navbar-nav mr-auto">
+                             <li class="nav-item active" style="padding-left:2em;">
+                                 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                             </li>
+                             <li class="nav-item active" style="padding-left:2em;">
+                                 <a class="nav-link" href="#">Products <span class="sr-only"></span></a>
+                             </li>
+                             <li class="nav-item active" style="padding-left:2em;">
+                                 <a class="nav-link" href="#">Clients <span class="sr-only"></span></a>
+                             </li>
+                             <li class="nav-item active" style="padding-left:2em;">
+                                 <a class="nav-link" href="#">About Us <span class="sr-only"></span></a>
+                             </li>
+                             <li class="nav-item active" style="padding-left:2em;">
+                                 <a class="nav-link" href="#">Contact Us <span class="sr-only"></span></a>
+                             </li>
+                         </ul>
+                     </div>
+                 </nav>
+             </div>
+         </div>
+         <div class="clearfix">
+            <div class="float-right">
+                <div class="homelogo">
+                    <img src="{{asset('img/Home-logo.svg')}}" alt="" style="height:30em;">
+                </div>
+            </div>
+            <div class="float-left col-md-6">
+                <div class="row">
+                    <div class="tagline">
+                        Managing your Comapny's <b>Health</b> and <b>Safety</b> with Industry leading Proffesionals.
+                    </div>
+                </div>
+                <a href="http://plutopro.uk/login"><div class="homebtn">Login to our Education Platform</div></a>
+                <a href="http://"><div class="homebtn">Login to COVID-Tracker Platform</div></a>
+            </div>
+         </div>
+        <div class="homeboxCurve"></div>
     </div>
-    <nav class="nav-home navbar-expand-lg navbar-light">
-        <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div id="my-nav" class="collapse navbar-collapse">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active" style="padding-left:2em;">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item active" style="padding-left:2em;">
-                    <a class="nav-link" href="#">Products <span class="sr-only"></span></a>
-                </li>
-                <li class="nav-item active" style="padding-left:2em;">
-                    <a class="nav-link" href="#">Clients <span class="sr-only"></span></a>
-                </li>
-                <li class="nav-item active" style="padding-left:2em;">
-                    <a class="nav-link" href="#">About Us <span class="sr-only"></span></a>
-                </li>
-                <li class="nav-item active" style="padding-left:2em;">
-                    <a class="nav-link" href="#">Contact Us <span class="sr-only"></span></a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+ </div>
 
-        <div class="tagline">
-            Health is our priority
-        </div>
+
+
+
+
+    <div class="home-footer">
+            <div class="p-2 ">Pluto Pro &copy; 2020 | Engineered by <a href="#">Innovus.lk</a></div>
     </div>
 
-
-
-
-
-    <footer class="mt-5">
-        <div class="d-flex justify-content-between  main-container">
-            <div class="p-2 bd-highlight">Pluto Pro &copy; 2020</div>
-            <div class="p-2 bd-highlight">Engineered by <a href="#">Innovus.lk</a></div>
-        </div>
-    </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
